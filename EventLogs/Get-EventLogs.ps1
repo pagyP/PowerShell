45c5731fe1ab75ceeb56abe0c8e6fba7b5c2b1e1
Paul Paginton <34237608+pagyP@users.noreply.github.com>
2018-10-15 15:54:13 +0100

    updated get-eventlogs with ad server query

--- EventLogs/Get-EventLogs.ps1
+++ EventLogs/Get-EventLogs.ps1
@@ -1,4 +1,7 @@
-
+#To run against servers in a domain review the below examples
+#Get all enabled servers that are in the domain.
+#$Servers = Get-ADComputer -Filter {(OperatingSystem -like "*Server*") -and (Enabled -eq $true)} -Properties OperatingSystem | select -ExpandProperty Name | Sort-Object
+#At the moment the the below line pulls in a text file with server names.  The above query can be used instead in a domain environment
 $servers = Get-Content -Path c:\servers.txt 
 $date = (get-date).AddDays(-4)
 #Get the critical and error level events from the Application and System logs
