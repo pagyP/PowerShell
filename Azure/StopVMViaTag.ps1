549591f6640c919357970c99b11b49d8b3f2ed67
Paul Paginton <pagyP@users.noreply.github.com>
2019-05-13 08:46:26 +0100

    comment out inmport module

--- Azure/StopVMViaTag.ps1
+++ Azure/StopVMViaTag.ps1
@@ -2,6 +2,6 @@
 #Example below
 #Get-AzureRmVM | where {$_.Tags['tag1'] -eq "value1" -and $_.Tags['tag2'] -eq "value2"}
 #Get all VMs in the subscription via the tag name 'shutDown' and a tag value of '19:00'
-Import-Module -Name AZ
+#Import-Module -Name AZ
 Get-AzVM | Where-Object {$_.tags['shutDown'] -eq "19:00"} | Stop-AZVM -Force -AsJob
 
