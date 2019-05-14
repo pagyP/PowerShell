409a0fc7124e014481101c330b34a1b0cb9ed32d
Paul Paginton <pagyP@users.noreply.github.com>
2019-05-14 16:44:37 +0100

    uses asjob switch now

--- Azure/StopVMViaTag.ps1
+++ Azure/StopVMViaTag.ps1
@@ -3,5 +3,10 @@
 #Get-AzureRmVM | where {$_.Tags['tag1'] -eq "value1" -and $_.Tags['tag2'] -eq "value2"}
 #Get all VMs in the subscription via the tag name 'shutDown' and a tag value of '19:00'
 #Import-Module -Name AZ
-Get-AzVM | Where-Object {$_.tags['shutDown'] -eq "19:00"} | Stop-AZVM -Force 
+#$start = Get-Date
+Get-AzVM | Where-Object {$_.tags['shutDown'] -eq "19:00"} | Stop-AZVM -Force -asjob
+get-job | Wait-Job
+#$end = Get-Date
+#Write-Host ("Stopping took {0}" -f ($end - $start))
+
 
