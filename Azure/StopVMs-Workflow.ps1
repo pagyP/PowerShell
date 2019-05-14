9782308fbc61b882ac4ec05783292436017bbaed
Paul Paginton <pagyP@users.noreply.github.com>
2019-05-14 14:24:44 +0100

    used az module instead

--- Azure/StopVMs-Workflow.ps1
+++ Azure/StopVMs-Workflow.ps1
@@ -1,9 +1,9 @@
 Workflow test {
 
-    $vms = Get-AzurermVM | Where-Object {$_.tags['shutDown'] -eq "19:00"} 
+    $vms = Get-AzVM | Where-Object {$_.tags['shutDown'] -eq "19:00"} 
     foreach -parallel ($vm in $vms) {
         #The following commands will be executed in parallel
-        Stop-AZurermVM -Force   
+        Stop-AZVM -Force   
     }
 }
 test
